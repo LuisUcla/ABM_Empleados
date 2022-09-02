200f346a103ba791810db31838daad47642dbfe0
LuisUcla <caluigi7@gmail.com>
2022-09-02 15:53:17 -0400

    configuracion de angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+
+import { AddEditEmpleadoComponent } from './components/add-edit-empleado/add-edit-empleado.component';
+import { ListEmpleadoComponent } from './components/list-empleado/list-empleado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MensajeConfirmacionComponent } from './components/share/mensaje-confirmacion/mensaje-confirmacion.component';
+import { AngularMaterialModule } from './components/angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditEmpleadoComponent,
+    ListEmpleadoComponent,
+    NavbarComponent,
+    MensajeConfirmacionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
